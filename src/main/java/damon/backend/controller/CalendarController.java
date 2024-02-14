e2d5c13cdaf0b4a68c9f9bfee4407c007f31f4b1
jungbin97 <ljb5054@gmail.com>
2024-02-14 16:16:52 +0900

    fix : 일정 컨트롤러 공통 응답으로 수정

--- src/main/java/damon/backend/controller/CalendarController.java
+++ src/main/java/damon/backend/controller/CalendarController.java
@@ -1,5 +1,6 @@
 package damon.backend.controller;
 
+import damon.backend.dto.Result;
 import damon.backend.dto.request.calendar.CalendarCreateRequestDto;
 import damon.backend.dto.request.calendar.CalendarEditRequestDto;
 import damon.backend.dto.request.calendar.CalendarsDeleteRequestDto;
@@ -29,74 +30,86 @@ public class CalendarController {
     @Operation(summary = "내 일정 등록", description = "내 일정을 등록합니다.")
     @ApiResponse(responseCode = "200", description = "일정 등록 성공")
 //    @ApiResponse(responseCode = "400", description = "일정 등록 실패")
-    public CalendarCreateResponseDto createCalendar(@RequestBody CalendarCreateRequestDto calendarCreateRequestDto) {
+    public Result<CalendarCreateResponseDto> createCalendar(@RequestBody CalendarCreateRequestDto calendarCreateRequestDto) {
         // 로그인 구현 후 member Id 추후 수정
         String memberId = "1";
 
-        return calendarService.createCalendar(memberId, calendarCreateRequestDto);
+        CalendarCreateResponseDto calendarCreateResponseDto = calendarService.createCalendar(memberId, calendarCreateRequestDto);
+
+        return Result.success(calendarCreateResponseDto);
     }
 
     @Operation(summary = "내 일정 리스트 조회", description = "내 일정리스트를 조회합니다.")
     @ApiResponse(responseCode = "200", description = "일정 리스트 조회 성공")
     @GetMapping("/my/calendar")
-    public Page<CalendarsResponseDto> getCalendars(
+    public Result<Page<CalendarsResponseDto>> getCalendars(
             @Schema(description = "페이지 번호(0부터 N까지)", defaultValue = "0")
             @RequestParam(name = "page", defaultValue = "0") int page,
 
             @Schema(description = "페이지에 출력할 개수를 입력합니다.", defaultValue = "10")
-            @RequestParam(name = "size",defaultValue = "10") int size
+            @RequestParam(name = "size", defaultValue = "10") int size
     ) {
 //        TODO: 로그인 구현 후 member Id 추후 수정
         String memberId = "1";
-        return calendarService.getCalendars(memberId, page, size);
+        Page<CalendarsResponseDto> calendarPages = calendarService.getCalendars(memberId, page, size);
+
+        return Result.success(calendarPages);
     }
 
     @Operation(summary = "내 일정 상세 조회", description = "내 일정 상세 정보를 조회합니다.")
     @ApiResponse(responseCode = "200", description = "일정 상세 조회 성공")
     @GetMapping("/my/calendar/{calendarId}")
-    public CalendarResponseDto getCalendar(
-            @Schema(description = "조회 할 일정 상세 페이지 ID", example="1")
+    public Result<CalendarResponseDto> getCalendar(
+            @Schema(description = "조회 할 일정 상세 페이지 ID", example = "1")
             @PathVariable("calendarId") Long calendarId
     ) {
 //         TODO: 로그인 구현 후 member Id 추후 수정
         String memberId = "1";
-        return calendarService.getCalendar(memberId, calendarId);
+        CalendarResponseDto calendarResponseDto = calendarService.getCalendar(memberId, calendarId);
+
+        return Result.success(calendarResponseDto);
     }
 
     @Operation(summary = "내 일정 상세 수정", description = "내 일정 상세 정보를 수정합니다.")
     @ApiResponse(responseCode = "200", description = "일정 수정 성공")
     @PutMapping("/calendar/{calendarId}")
-    public CalendarEditResponseDto updateCalendar(
-            @Schema(description = "수정 할 일정 상세 페이지 ID", example="1")
+    public Result<CalendarEditResponseDto> updateCalendar(
+            @Schema(description = "수정 할 일정 상세 페이지 ID", example = "1")
             @PathVariable("calendarId") Long calendarId,
 
             @RequestBody CalendarEditRequestDto calendarEditRequestDto
     ) {
 //        TODO: 로그인 구현 후 member Id 추후 수정
         String memberId = "1";
-        return calendarService.updateCalendar(memberId, calendarId, calendarEditRequestDto);
+        CalendarEditResponseDto calendarEditResponseDto = calendarService.updateCalendar(memberId, calendarId, calendarEditRequestDto);
+
+        return Result.success(calendarEditResponseDto);
     }
 
     @Operation(summary = "내 일정 삭제", description = "내 일정을 삭제합니다.")
     @ApiResponse(responseCode = "200", description = "일정 삭제 성공")
     @DeleteMapping("/calendar/{calendarId}")
-    public void deleteCalendar(
-            @Schema(description = "삭제 할 일정 상세 페이지 ID", example="1")
+    public Result<String> deleteCalendar(
+            @Schema(description = "삭제 할 일정 상세 페이지 ID", example = "1")
             @PathVariable("calendarId") Long calendarId
     ) {
 //        TODO: 로그인 구현 후 member Id 추후 수정
         String memberId = "1";
         calendarService.deleteCalendar(memberId, calendarId);
+
+        return Result.success("내 일정 삭제 성공");
     }
 
     @Operation(summary = "내 일정 선택 삭제", description = "내 일정을 선택 삭제합니다.")
     @ApiResponse(responseCode = "200", description = "일정 선택 삭제 성공")
     @DeleteMapping("/calendar")
-    public void deleteCalendars(
+    public Result<String> deleteCalendars(
             @RequestBody CalendarsDeleteRequestDto calendarsDeleteRequestDto
     ) {
 //        TODO: 로그인 구현 후 member Id 추후 수정
         String memberId = "1";
         calendarService.deleteCalendars(memberId, calendarsDeleteRequestDto);
+
+        return Result.success("내 일정 선택 삭제 성공");
     }
 }
