7a92c7647f36e7ca242f9e52fe47ba83954b764e
user <315luz10@gmail.com>
2024-02-15 03:52:40 +0900

    refactor :  리뷰 리팩토링 완료

--- src/main/java/damon/backend/repository/TravelRepository.java
+++
@@ -1,9 +0,0 @@
-package damon.backend.repository;
-
-import damon.backend.entity.Travel;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface TravelRepository extends JpaRepository<Travel, Long> {
-}
